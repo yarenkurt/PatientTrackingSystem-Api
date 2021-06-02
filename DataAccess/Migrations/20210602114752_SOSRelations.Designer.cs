01c70c29ef1ef8369f86007844ccc8493cc75d92
Yaren <yarenkurt078@outlook.com>
2021-06-02 14:49:45 +0300

    Commit

--- DataAccess/Migrations/20210602114752_SOSRelations.Designer.cs
+++ DataAccess/Migrations/20210602114752_SOSRelations.Designer.cs
@@ -4,14 +4,16 @@ using DataAccess.Contexts.EF;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(PatientTrackingContext))]
-    partial class PatientTrackingContextModelSnapshot : ModelSnapshot
+    [Migration("20210602114752_SOSRelations")]
+    partial class SOSRelations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DataAccess/Migrations/20210602114752_SOSRelations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class SOSRelations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            /*
+             migrationBuilder.DropColumn(
+                name: "Time",
+                table: "Appointments");
+             */
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "Time",
+                table: "Appointments",
+                type: "time",
+                nullable: false,
+                defaultValueSql: "'00:00'");
+        }
+    }
+}
